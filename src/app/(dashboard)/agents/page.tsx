cc247390efa3bc5115dcaec15f812c807e194c0b
neelmajmudar <neel.majmudar@gmail.com>
2025-06-20 17:15:11 -0700

    11-agentsform

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -8,10 +8,24 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { Suspense } from "react";
 
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+      });
+    
+    if (!session) {
+          redirect("/sign-in");
+    } 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
     return (
+         <>
+         <AgentsListHeader/>
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<LoadingState title="Loading Agents" description="This may take a few seconds"/>}>
                 <ErrorBoundary fallback={<AgentsViewError/>}>
@@ -19,6 +33,7 @@ const Page = async () => {
                 </ErrorBoundary>
             </Suspense>
         </HydrationBoundary>
+        </>
     );
 };
 
